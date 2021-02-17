b9d230311ef274f75911638c5eb07e5ecc74ee2c
Erin <erinpwoo@yahoo.com>
2021-02-16 18:38:03 -0800

    Time start/end validation

--- app/models/poll.rb
+++ app/models/poll.rb
@@ -2,4 +2,11 @@ class Poll < ApplicationRecord
   validates :title, presence: true
   validates :timeframe_start, presence: true
   validates :timeframe_end, presence: true
+  validate :validateTimeframe
+
+  def validateTimeframe
+    if timeframe_start.nil? || timeframe_end.nil? || (timeframe_start > timeframe_end)
+      errors.add(:timeframe_start, "must be before end time")
+    end
+  end
 end
