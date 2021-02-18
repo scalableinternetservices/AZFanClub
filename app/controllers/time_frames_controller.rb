727f997b99077fd1b946074b56f7725f40dc4757
AndrewZhang17 <zhang.w.andrew@gmail.com>
2021-02-17 18:07:57 -0800

    restrict time frame params

--- app/controllers/time_frames_controller.rb
+++ app/controllers/time_frames_controller.rb
@@ -2,7 +2,11 @@ class TimeFramesController < ApplicationController
     def create
         @poll = Poll.find(params[:poll_id])
         @user = User.find(params[:user_id])
-        @user.time_frames.create!({start_time: params[:time_frame][:start_time], end_time: params[:time_frame][:end_time], user_id: @user.id})
+        @user.time_frames.create!({start_time: time_frame_params[:start_time], end_time: time_frame_params[:end_time], user_id: @user.id})
         redirect_to poll_user_path(@poll, @user)
     end
+
+    def time_frame_params
+        params.require(:time_frame).permit(:start_time, :end_time)
+      end
 end
