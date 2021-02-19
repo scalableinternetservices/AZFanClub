3fa98e5cb03a44a70ded2d23d9874a262dfb6942
Erin <erinpwoo@yahoo.com>
2021-02-18 16:56:18 -0800

    Timeframe can be specified when creating a user

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,11 +1,13 @@
 class UsersController < ApplicationController
     def create
         @poll = Poll.find(params[:poll_id])
-        @user = @poll.users.create(user_params)
+        @user = @poll.users.create!(user_params)
         redirect_to poll_path(@poll)
       end
 
     def user_params
-      params.require(:user).permit(:name, :time_frames_attributes => [:start_time, :end_time] )
+      time_frame_params = [params.permit(time_frames: [:start_time, :end_time])[:time_frames]]
+      p time_frame_params
+      {name: params[:user][:name], time_frames_attributes: time_frame_params}
     end
 end
