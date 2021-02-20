ee9714eb815977dbda6fef495e37a0bd4b384037
ChinmaySonar <chinmaysonar96@gmail.com>
2021-02-19 22:33:55 -0800

    add poll timeframe validation

--- app/models/time_frame.rb
+++ app/models/time_frame.rb
@@ -8,6 +8,15 @@ class TimeFrame < ApplicationRecord
     if start_time.nil? || end_time.nil? || (start_time > end_time)
       errors.add(:start_time, "must be before end time")
     end
+    poll = Poll.find(user.poll_id)
+    if !(poll.timeframe_start <= start_time && start_time <= poll.timeframe_end)
+      errors.add(:start_time, "must be inside poll time")
+    end
+    if !(poll.timeframe_start <= end_time && end_time <= poll.timeframe_end)
+      errors.add(:end_time, "must be inside poll time")
+    end
+    #logger.debug "PRINTING POLL" + @poll
+    #logger.debug "Poll object" + .to_s
   end
 
 end
