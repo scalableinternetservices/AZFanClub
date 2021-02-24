86f83b96db1990934062200f2695ed85a32ab2d3
ChinmaySonar <chinmaysonar96@gmail.com>
2021-02-23 22:44:40 -0800

    add timeframe error messages

--- app/controllers/time_frames_controller.rb
+++ app/controllers/time_frames_controller.rb
@@ -2,9 +2,18 @@ class TimeFramesController < ApplicationController
     def create
         @poll = Poll.find(params[:poll_id])
         @user = User.find(params[:user_id])
+        @time_frame = @user.time_frames.create({start_time: time_frame_params[:start_time], end_time: time_frame_params[:end_time], user_id: @user.id, tier: time_frame_params[:tier]})
+        #redirect_to poll_user_path(@poll, @user)
+        respond_to do |format|
+            if @time_frame.save
+              format.html { redirect_to poll_user_path(@poll, @user) , notice: "Time frame successfully created." }
+              format.json { render :show, status: :created, location: @user }
+            else
+              format.html { redirect_to poll_user_path(@poll, @user), alert: @time_frame.errors.full_messages }
+              format.json { render json: @time_frame.errors }
+            end
+          end
 
-        @user.time_frames.create!({start_time: time_frame_params[:start_time], end_time: time_frame_params[:end_time], user_id: @user.id, tier: time_frame_params[:tier]})
-        redirect_to poll_user_path(@poll, @user)
     end
 
     def time_frame_params
