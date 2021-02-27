4c4f2ea89ffe6b280587796a5368f1fa89f68c22
Erin <erinpwoo@yahoo.com>
2021-02-26 18:48:50 -0800

    Updated timeframe validation, deleted time frame count

--- app/models/time_frame.rb
+++ app/models/time_frame.rb
@@ -6,11 +6,11 @@ class TimeFrame < ApplicationRecord
   validates :tier, inclusion: { in: 1..3, message: "must be 1, 2, or 3" }
 
   def validateTimeframe
+    poll = Poll.find(user.poll_id)
     if start_time.nil? || end_time.nil? || (start_time > end_time)
       errors.add(:start_time, "must be before end time")
     
     else
-      poll = Poll.find(user.poll_id)
       if !(poll.timeframe_start <= start_time && start_time <= poll.timeframe_end)
         errors.add(:start_time, "must be inside poll time")
       end
@@ -19,11 +19,11 @@ class TimeFrame < ApplicationRecord
       end
     end
 
-    if !(poll.daily_start <= start_time.hour) 
+    if start_time.nil? || !(poll.daily_start <= start_time.hour) 
       errors.add(:start_time, "must be after daily start time")
     end
 
-    if !(poll.daily_end > end_time.hour)
+    if end_time.nil? || !(poll.daily_end > end_time.hour)
       errors.add(:end_time, "must be before daily end time")
     end
 
